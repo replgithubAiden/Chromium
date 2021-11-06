2a9c068acd4653b5410be1c06b88327ba1078c17
replgithubAiden <92238811+replgithubAiden@users.noreply.github.com>
2021-11-06 17:21:42 -0400

    Create Chromium.py

---
+++ Chromium.py
@@ -0,0 +1,10 @@
+import time
+from webbot import Browser
+time.sleep(1)
+print("Installed")
+time.sleep(2)
+print("Launching")
+Chromium = Browser()
+print("Finalizing")
+time.sleep(1)
+print("Finishe
